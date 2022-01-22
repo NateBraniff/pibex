c6388a5be0d8e087081a57085215eacac673793b
Nate Braniff <nbraniff@uwaterloo.ca>
2022-01-21 18:43:09 -0800

    created pibex and test files, others

---
+++ pibex.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+class model():
+    """Model class for the pibex package."""
+
+    def __init__(self, f, link, dist):
+        """Constructor for the pibex model class"""
+        self.f = f
+        self.link = link
+        self.dist = dist
+
+    def simulate(self, x):
+        """pibex function for simulating data"""
+
+        u=x.apply(self.f,'index')
+
+
