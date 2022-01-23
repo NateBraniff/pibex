e40dfa2f22dfa5459aa877db7e2f912ef9e6c748
Nate Braniff <nbraniff@uwaterloo.ca>
2022-01-22 16:47:38 -0800

    additions to simulate() prototype

--- examples/test.py
+++ examples/test.py
@@ -1,2 +1,20 @@
-import pibex
+import pibex as pbx
+import numpy as np
+import pandas as pd
 
+
+f = lambda x: [1, x, x**2]
+nf=3
+link = lambda x: np.log(x)
+ilink = lambda x: np.exp(x)
+dist = 'norm'
+
+model = pbx.Model(f,nf,link,ilink,dist)
+
+
+x=pd.DataFrame({'inputs':[-1,0,1]})
+
+
+u = model.simulate(x)
+
+print(u)
--- pibex/model.py
+++ pibex/model.py
@@ -4,15 +4,23 @@ import pandas as pd
 class Model():
     """Model class for the pibex package"""
 
-    def __init__(self, f, link, dist):
+    def __init__(self, f, nf, link, ilink, dist):
         """Constructor for the pibex model class"""
         self.f = f
         self.link = link
         self.dist = dist
+        self.param = np.zeros(nf)
 
     def simulate(self, x):
         """pibex function for simulating data"""
 
-        u=x.apply(self.f,'index')
+        #compute design matrix
+        F = x.apply(self.f,axis='index',result_type='expand')
+        #dot design matrix with params
+        FB = F.dot(self.param)
+        #compute expected mu
+        u = self.ilink(FB)
+
+        return u
 
 
